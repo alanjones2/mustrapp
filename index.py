7688315946c956404f97599a948215816626439c
Alan Jones <jones.alan@hotmail.co.uk>
2021-12-02 16:49:17 +0100

    add licence to code

--- index.py
+++ index.py
@@ -1,6 +1,22 @@
 ##################################################
 # Mustrapp - Multi-app Streamlit Application
 #
+#
+# Copyright 2021 Alan Jones (https://alanjones2.github.io/)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+
 # Do not change this code except for the message below
 # which can be plain text or any markdown code and will
 # be displayed above the selection box
